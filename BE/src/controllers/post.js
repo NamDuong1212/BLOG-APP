348176461f314b73f4af1efa20dcde8b86e4df23
tobacoguys <dungchelsea1723@gmail.com>
2024-09-09 12:06:31 +0700

    api get post

--- BE/src/controllers/post.js
+++ BE/src/controllers/post.js
@@ -32,6 +32,38 @@ const postCTL = {
             });
         }
     },
+
+    getPostById: async (req, res) => {
+        try {
+            const { postID } = req.params;
+            const post = await postModel.findById(postID);
+            if (!post) return res.status(404).json({ message: 'Post not found' });
+            res.json(post);
+          } catch (error) {
+            res.status(500).json({ message: 'Failed to fetch post' });
+          }
+    },
+
+    update: async (req, res) => {
+        const { title, content, author } = req.body;
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id,
+                    { title, content, author },
+                    { new: true }
+                );
+                
+                if (!updatedPost) 
+                    return res.status(404).json({ 
+                        message: 'Post not found' 
+                    });
+                    
+                res.json(updatedPost);
+                } catch (error) {
+                    res.status(500).json({ 
+                        message: 'Failed to update post' 
+                    });
+                }
+    }
 }
 
 export default postCTL;
--- BE/src/routes/post.js
+++ BE/src/routes/post.js
@@ -4,5 +4,11 @@ import UserMDW from "../middlewares/users.js";
 const postRouter = Router();
 
 postRouter.post('/create/:userID', UserMDW.validateToken, UserMDW.isAdmin, postCTL.create)
-postRouter.get('/getpost', UserMDW.validateToken, postCTL.create)
+
+postRouter.get('/getPost', postCTL.getPost)
+postRouter.get('/getPostByID/:postID', postCTL.getPostById)
+
+postRouter.put('/updatePost/:postID', UserMDW.validateToken, UserMDW.isAdmin, postCTL.update)
+
+
 export default postRouter;
